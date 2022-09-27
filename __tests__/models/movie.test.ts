ae132899ed880eb8ec5dd9df4b3fb2e62a77631e
Alvaro <alvaro99.cunha@gmail.com>
2022-09-27 19:26:38 -0300

    added tests

---
+++ __tests__/models/movie.test.ts
@@ -0,0 +1,19 @@
+import { Movie } from '../../src/models/movie'
+
+test('valid movie', () => {
+  const movieTemp = {
+    name: 'Filme',
+    director: 'Diretor',
+    year: 2000,
+    actors: [
+      'Ator 1',
+      'Ator 2',
+      'Ator 3'
+    ]
+  }
+  const validMovie = Movie.build(movieTemp)
+  expect(validMovie.name).toBe(movieTemp.name)
+  expect(validMovie.director).toBe(movieTemp.director)
+  expect(validMovie.year).toBe(movieTemp.year)
+  expect(validMovie.actors).toBe(movieTemp.actors)
+})
